1cc0fe6b9479daa64505de3a30d9948f892f3787
Hamed Noroozi <noruzi206@gmail.com>
2024-02-02 13:23:49 +0330

    add event handler for edit of each todo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const addButton = document.getElementById("add-button");
+const editButton= document.getElementById("edit-button");
 const alertMessage = document.getElementById("alert-message");
 const todosBody = document.querySelector("tbody")
 const deleteAll = document.getElementById("delete-all-button");
@@ -38,8 +39,8 @@ const displayTodos = () => {
           <td>${todo.date || "No date"}</td>
           <td>${todo.compeleted ? "done" : "pending"}</td>
           <td>
-             <button>Edit</button>
-             <button onclick="toggleHandler('${todo.id}')">${todo.compeleted ? "Undo": "Do"}</button>
+             <button onclick="editHandler('${todo.id}')">Edit</button>
+             <button onclick="toggleHandler('${todo.id}')">${todo.compeleted ? "Undo" : "Do"}</button>
              <button onclick="deleteHandler('${todo.id}')">Delete</button>
           </td>
       </tr>
@@ -93,11 +94,20 @@ const deleteHandler = (id) => {
 }
 
 const toggleHandler = (id) => {
-    const todo = todos.find(todo =>todo.id === id);
-    todo.compeleted = !todo.compeleted;
-    saveToLocalStorage();
-    displayTodos();
-    showAlert("Todo status changed successfully","success");
+   const todo = todos.find(todo => todo.id === id);
+   todo.compeleted = !todo.compeleted;
+   saveToLocalStorage();
+   displayTodos();
+   showAlert("Todo status changed successfully", "success");
+}
+
+const editHandler = (id) => {
+   const todo=todos.find(todo => todo.id === id);
+   taskInput.value=todo.task;
+   dateInput.value=todo.date;
+   addButton.style.display="none";
+   editButton.style.display="inline-block";
+
 }
 
 window.addEventListener("load", displayTodos)
